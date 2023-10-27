ccd0dd20b6674262af2d5337129f5dbfa04f99d1
mobyw <mobyw66@gmail.com>
2023-10-27 19:59:03 +0800

    :fire: 移除旧版程序

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM tiangolo/uvicorn-gunicorn-fastapi:python3.8
-
-RUN python3 -m pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
-
-COPY . /app/
-
-RUN python3 -m pip install -r requirements.txt
-
-CMD nb run
---
+++ src/nonebot_plugin_rss/__init__.py
@@ -0,0 +1,25 @@
+from nonebot.log import logger
+from nonebot import require, get_driver
+from nonebot.plugin import PluginMetadata
+
+require("nonebot_plugin_apscheduler")
+
+
+VERSION = "2.6.21"
+
+__plugin_meta__ = PluginMetadata(
+    name="ELF_RSS",
+    description="RSS 订阅插件，订阅源建议选择 RSSHub",
+    usage="https://github.com/Quan666/ELF_RSS",
+    type="application",
+    homepage="https://github.com/Quan666/ELF_RSS",
+    supported_adapters=None,
+    extra={"author": "Quan666 <i@Rori.eMail>", "version": VERSION},
+)
+
+driver = get_driver()
+
+
+@driver.on_startup
+async def startup():
+    logger.info("ELF_RSS 订阅器启动成功！")
